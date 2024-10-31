e9169a72ac986e83bdbcd6e7c48b71c4785ed7a5
ai-n0808 <aichan.88.aichan@gmail.com>
2024-10-31 17:53:41 +0900

    feat: add get request

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+//Need get request
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
 app.listen(8080, () => {
   console.log(`Server is running`);
 });
