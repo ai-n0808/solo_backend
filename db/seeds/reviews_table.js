7c47124ef18d5f2262dea1e9f68a27388f18b1ea
ai-n0808 <aichan.88.aichan@gmail.com>
2024-11-02 18:31:01 +0900

    feat: add row in reviews table

--- db/seeds/reviews_table.js
+++ db/seeds/reviews_table.js
@@ -1,13 +1,17 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+exports.seed = async function (knex) {
+  await knex("reviews_table").del();
+  await knex("reviews_table").insert([
+    {
+      id: 1,
+      user_id: 1,
+      game_id: 8,
+      rating: 5,
+      review: "My favorite",
+      created_at: new Date(),
+    },
   ]);
 };
