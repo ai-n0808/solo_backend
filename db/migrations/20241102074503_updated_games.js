3c0cab164d45769d58da14d82434531b519fbf5e
ai-n0808 <aichan.88.aichan@gmail.com>
2024-11-02 16:57:26 +0900

    feat: add new column

---
+++ db/migrations/20241102074503_updated_games.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table("games_table", (table) => {
+    table.integer("generation");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table("games_table", (table) => {
+    table.dropColumn("generation");
+  });
+};
