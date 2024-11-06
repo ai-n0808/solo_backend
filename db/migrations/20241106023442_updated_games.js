514f2050eb48c5345ed4b21e340b8d764cab0373
ai-n0808 <aichan.88.aichan@gmail.com>
2024-11-06 12:20:05 +0900

    feat: add image column

---
+++ db/migrations/20241106023442_updated_games.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("games_table", (table) => {
+    table.string("image");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("games_table", (table) => {
+    table.dropColumn("image");
+  });
+};
